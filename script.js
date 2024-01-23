4632e630f344c738b344602483c2fe4eaaf54f27
LucasTangerino <lucastangerino13@gmail.com>
2024-01-23 12:33:05 -0300

    foi feita a refatoração e abstração do codigo

---
+++ script.js
@@ -0,0 +1,41 @@
+// script.js
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function atualizaData() {
+    const dataAtual = new Date();
+    const dia = dataAtual.getDate();
+    const mes = ("0" + (dataAtual.getMonth() + 1)).slice(-2);
+    const ano = dataAtual.getFullYear();
+
+    document.getElementById("data").innerHTML = `${dia}/${mes}/${ano}`;
+}
+
+setInterval(atualizaData, 1000);
+
+function playWhatsAppSound() {
+    var audio = document.getElementById('whatsAppClickSound');
+    audio.play();
+}
+
+function playDiscordSound() {
+    var audio = document.getElementById('discordClickSound');
+    audio.play();
+}
