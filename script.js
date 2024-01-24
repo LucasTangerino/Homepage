0b403f98707c7be226a0cd3144b248dccd328d39
LucasTangerino <lucastangerino13@gmail.com>
2024-01-24 16:40:32 -0300

    foi adicionado opção de ver a temperatura em cidade

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // script.js
-
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -12,11 +11,12 @@ function startTime() {
     t = setTimeout(startTime, 500);
 }
 
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
+
+function checkTime(time) {
+    if (time < 10) {
+        time = "0" + time;
     }
-    return i;
+    return time;
 }
 
 function atualizaData() {
@@ -39,3 +39,57 @@ function playDiscordSound() {
     var audio = document.getElementById('discordClickSound');
     audio.play();
 }
+
+const apiKey ="aabf385f061bdd5d53d344356fae7b2e";
+
+// https://flagsapi.com/
+
+const getFlag = (pais) => {
+    return "https://flagsapi.com/" + pais + "/flat/64.png";
+}
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperatura span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const humidityElement = document.querySelector("#humidity span")
+const windElement = document.querySelector("#wind span")
+
+const getWeatherData = async(city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    console.log(data)
+    return data;
+}
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city); // espera aqui
+
+    console.log(typeof(data.main.temp))
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = data.main.temp;
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    countryElement.setAttribute("src", getFlag(data.sys.country));
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}Km/h`
+    
+}
+
+searchBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+})
