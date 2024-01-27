de112e710c78a475f8c00e64651909d3cfd5e78c
LucasTangerino <lucastangerino13@gmail.com>
2024-01-27 11:34:58 -0300

    adiconado foto para o background no darkmode

--- script.js
+++ script.js
@@ -94,22 +94,39 @@ searchBtn.addEventListener("click", (e) =>{
     showWeatherData(city);
 })
 
-var images = ["imagens/images.jpg","imagens/images2.jpg","imagens/images3.jpg"];
+var images = ["imagens/images.jpg","imagens/images2.jpg","imagens/images3.jpg","imagens/images4.jpg","imagens/images5.jpg","imagens/images6.jpg"];
 var currentIndex = 0;
+var timeoutId;
 
 function changeBackground() {
-    document.body.style.backgroundImage = "url('" + images[currentIndex] + "')";
+    const body = document.querySelector('body');
+    var btn = document.querySelector(".dark-mode-btn");
+    if (body.classList.contains('dark-mode')) {
+        images = ["imagens/images4.jpg", "imagens/images5.jpg","imagens/images6.jpg"];
+        timeoutId = setTimeout(changeBackground, 10000);
+    } else {
+        images = ["imagens/images.jpg","imagens/images2.jpg","imagens/images3.jpg"];
+        timeoutId = setTimeout(changeBackground, 10000);
+    }
+    body.style.backgroundImage = "url('" + images[currentIndex] + "')";
     currentIndex = (currentIndex + 1) % images.length;
-    setTimeout(changeBackground, 5000); 
 }
 
 window.addEventListener('load', function() {
     startTime();
     changeBackground();
 });
+
 function toggleDarkMode() {
     const body = document.querySelector('body');
     var btn = document.querySelector(".dark-mode-btn");
     body.classList.toggle('dark-mode');
     btn.classList.toggle("dark-mode");
-  }
+    clearTimeout(timeoutId);
+    changeBackground();
+}
+
+
+
+
+
